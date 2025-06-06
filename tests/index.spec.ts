295f3247bc96f0a4654afe8f96a9565595f5653d
Anshul Gupta <ansg191@anshulg.com>
2025-06-06 16:58:53 -0700

    Add `Uptime` tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -4,6 +4,7 @@ import {
   type Locator,
   type BrowserContext,
 } from "@playwright/test";
+import { differenceCiede2000, parse } from "culori";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/");
@@ -14,7 +15,7 @@ test("has title", async ({ page }) => {
 });
 
 test("has header", async ({ page }) => {
-  let header = page.locator("header");
+  const header = page.locator("header");
   await expect(header).toBeVisible();
   await expect(header.locator("div").first()).toHaveText("ANSHUL GUPTA (7)");
   await expect(header.locator("div").last()).toHaveText("ANSHUL GUPTA (7)");
@@ -24,15 +25,59 @@ test("has header", async ({ page }) => {
 });
 
 test("has Email link", async ({ page }) => {
-  let emailLink = page.getByRole("link", { name: "ansg191@anshulg.com" });
+  const emailLink = page.getByRole("link", { name: "ansg191@anshulg.com" });
   await expect(emailLink).toBeVisible();
   await expect(emailLink).toHaveText("ansg191@anshulg.com");
   await expect(emailLink).toHaveAttribute("href", "mailto:ansg191@anshulg.com");
 });
 
 test("has GitHub link", async ({ page, context }) => {
-  let ghLink = page.getByRole("link", { name: "ansg191" }).nth(1);
+  const ghLink = page.getByRole("link", { name: "ansg191" }).nth(1);
   await checkLinkNewTab(context, ghLink, "https://github.com/ansg191");
+});
+
+test.describe("Uptime Indicator", () => {
+  test("has good Uptime indicator", async ({ page }) => {
+    await page.waitForLoadState("networkidle");
+
+    const link = page.getByRole("link", { name: "Git Server" });
+    await expect(link).toBeVisible();
+
+    const el = link.locator("span.absolute").nth(1);
+    await expect(el).toBeVisible();
+
+    // Check that the color is green
+    const bgColor = await el.evaluate(
+      (el) => getComputedStyle(el).backgroundColor,
+    );
+    const expColor = "oklch(72.3% 0.219 149.579)"; // green-500
+    expect(areColorsSimilar(expColor, bgColor)).toBe(true);
+
+    // Check that the Screen Reader text is correct
+    const srEl = link.locator("span.sr-only");
+    await expect(srEl).toHaveText("Service Status: Up");
+  });
+
+  test("has bad Uptime indicator", async ({ page }) => {
+    await page.waitForLoadState("networkidle");
+
+    const link = page.getByRole("link", { name: "Apt Repo" });
+    await expect(link).toBeVisible();
+
+    const el = link.locator("span.absolute").nth(1);
+    await expect(el).toBeVisible();
+
+    // Check that the color is red
+    const bgColor = await el.evaluate(
+      (el) => getComputedStyle(el).backgroundColor,
+    );
+    const expColor = "oklch(63.7% 0.237 25.331)"; // red-500
+    expect(areColorsSimilar(expColor, bgColor)).toBe(true);
+
+    // Check that the Screen Reader text is correct
+    const srEl = link.locator("span.sr-only");
+    await expect(srEl).toHaveText("Service Status: Down");
+  });
 });
 
 async function checkLinkNewTab(
@@ -45,4 +90,20 @@ async function checkLinkNewTab(
   await link.click();
   const newPage = await pagePromise;
   await expect(newPage).toHaveURL(expUrl);
+}
+
+function areColorsSimilar(
+  exp: string,
+  found: string,
+  threshold = 0.05,
+): boolean {
+  const parsedExp = parse(exp);
+  const parsedFound = parse(found);
+
+  if (!parsedExp || !parsedFound) {
+    throw new Error("Invalid color format.");
+  }
+
+  const deltaE = differenceCiede2000()(parsedExp, parsedFound);
+  return deltaE < threshold;
 }
